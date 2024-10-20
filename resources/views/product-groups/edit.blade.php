607ad009165fc8a17bcb3d30df0da1dbcb9ae9aa
er-ko <roman.kocian@gmail.com>
2024-10-20 11:02:33 +0100

    product_group_edit_view_update

--- resources/views/product-groups/edit.blade.php
+++ resources/views/product-groups/edit.blade.php
@@ -14,7 +14,7 @@
 						<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
 					</svg>
 				</button>
-				<a href="{{ route('category.index') }}" class="text-pink-600 hover:text-pink-700">
+				<a href="{{ route('product_group.index') }}" class="text-pink-600 hover:text-pink-700">
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
 					</svg>
