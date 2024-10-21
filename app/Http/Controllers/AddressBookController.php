35e79b64c82efbfd6e03901693b46560f4a128f1
er-ko <roman.kocian@gmail.com>
2024-10-21 15:24:45 +0100

    address_book_complete

--- app/Http/Controllers/AddressBookController.php
+++ app/Http/Controllers/AddressBookController.php
@@ -23,7 +23,7 @@ class AddressBookController extends Controller
             $id = $request->get('id');
 
             $output = Db::table('address_books_billing')
-                        ->select('company_name', 'first_name', 'last_name')
+                        ->select('company_name', 'first_name', 'last_name', 'city', 'phonecode', 'phone', 'email')
                         ->where('address_book_id', '=', $id)
                         ->orderBy('id', 'asc')
                         ->limit(1)
@@ -56,7 +56,7 @@ class AddressBookController extends Controller
         foreach ($request->billing_code as $key => $billing) {
 
             $phonecode  = substr($request->billing_phonecode[$key], 1, 3);
-            $phone      = substr(str_replace(' ', '', $request->billing_phone[$key]), 4, 9);
+            $phone      = str_replace(' ', '', $request->billing_phone[$key]);
 
             $data = [
                 'billing_code'          => $billing,
@@ -69,8 +69,8 @@ class AddressBookController extends Controller
                 'billing_address_ext'   => $request->billing_address_ext[$key],
                 'billing_postcode'      => $request->billing_postcode[$key],
                 'billing_city'          => $request->billing_city[$key],
-                'billing_phonecode'     => $phonecode[$key],
-                'billing_phone'         => $phone[$key],
+                'billing_phonecode'     => $phonecode,
+                'billing_phone'         => $phone,
                 'billing_email'         => $request->billing_email[$key],
             ];
 
@@ -114,7 +114,7 @@ class AddressBookController extends Controller
             foreach ($request->branch_code as $key => $branch) {
 
                 $phonecode  = substr($request->branch_phonecode[$key], 1, 3);
-                $phone      = substr(str_replace(' ', '', $request->branch_phone[$key]), 4, 9);
+                $phone      = str_replace(' ', '', $request->branch_phone[$key]);
 
                 $data = [
                     'branch_code'          => $branch,
@@ -202,7 +202,128 @@ class AddressBookController extends Controller
      */
     public function update(Request $request, AddressBook $addressBook)
     {
-        //
+        $validated = [
+            'due_date' => 7
+        ];
+        $addressBook->update($validated);
+        DB::table('address_books_billing')->where('address_book_id', '=', $addressBook->id)->delete();
+        foreach ($request->billing_code as $key => $billing) {
+
+            $phonecode  = substr($request->billing_phonecode[$key], 1, 3);
+            $phone      = str_replace(' ', '', $request->billing_phone[$key]);
+
+            $data = [
+                'billing_code'          => $billing,
+                'billing_company_name'  => $request->billing_company_name[$key],
+                'billing_company_id'    => $request->billing_company_id[$key],
+                'billing_vat_id'        => $request->billing_vat_id[$key],
+                'billing_first_name'    => $request->billing_first_name[$key],
+                'billing_last_name'     => $request->billing_last_name[$key],
+                'billing_address'       => $request->billing_address[$key],
+                'billing_address_ext'   => $request->billing_address_ext[$key],
+                'billing_postcode'      => $request->billing_postcode[$key],
+                'billing_city'          => $request->billing_city[$key],
+                'billing_phonecode'     => $phonecode,
+                'billing_phone'         => $phone,
+                'billing_email'         => $request->billing_email[$key],
+            ];
+
+            $validator = Validator::make($data, [
+                'billing_code'          => 'required|string|max:4',
+                'billing_company_name'  => 'string|max:128|nullable',
+                'billing_company_id'    => 'string|max:8|nullable',
+                'billing_vat_id'        => 'string|max:12|nullable',
+                'billing_first_name'    => 'required|string|max:128',
+                'billing_last_name'     => 'required|string|max:128',
+                'billing_address'       => 'required|string|max:255',
+                'billing_address_ext'   => 'string|max:255|nullable',
+                'billing_postcode'      => 'required|string|max:12',
+                'billing_city'          => 'required|string|max:128',
+                'billing_phonecode'     => 'required|string|max:3',
+                'billing_phone'         => 'required|string|max:9',
+                'billing_email'         => 'required|string|max:128',
+            ]);
+
+            if (!$validator->fails()) {
+                DB::table('address_books_billing')->insert([
+                    'address_book_id'   => $addressBook->id,
+                    'code'              => $data['billing_code'],
+                    'company_name'      => $data['billing_company_name'],
+                    'company_id'        => $data['billing_company_id'],
+                    'vat_id'            => $data['billing_vat_id'],
+                    'first_name'        => $data['billing_first_name'],
+                    'last_name'         => $data['billing_last_name'],
+                    'address'           => $data['billing_address'],
+                    'address_ext'       => $data['billing_address_ext'],
+                    'postcode'          => $data['billing_postcode'],
+                    'city'              => $data['billing_city'],
+                    'phonecode'         => $data['billing_phonecode'],
+                    'phone'             => $data['billing_phone'],
+                    'email'             => $data['billing_email'],
+                ]);
+            }
+        }
+
+        if (isset($request->branch_code)) {
+            DB::table('address_books_branch')->where('address_book_id', '=', $addressBook->id)->delete();
+            foreach ($request->branch_code as $key => $branch) {
+
+                $phonecode  = substr($request->branch_phonecode[$key], 1, 3);
+                $phone      = str_replace(' ', '', $request->branch_phone[$key]);
+
+                $data = [
+                    'branch_code'          => $branch,
+                    'branch_company_name'  => $request->branch_company_name[$key],
+                    'branch_company_id'    => $request->branch_company_id[$key],
+                    'branch_vat_id'        => $request->branch_vat_id[$key],
+                    'branch_first_name'    => $request->branch_first_name[$key],
+                    'branch_last_name'     => $request->branch_last_name[$key],
+                    'branch_address'       => $request->branch_address[$key],
+                    'branch_address_ext'   => $request->branch_address_ext[$key],
+                    'branch_postcode'      => $request->branch_postcode[$key],
+                    'branch_city'          => $request->branch_city[$key],
+                    'branch_phonecode'     => $phonecode,
+                    'branch_phone'         => $phone,
+                    'branch_email'         => $request->branch_email[$key],
+                ];
+    
+                $validator = Validator::make($data, [
+                    'branch_code'          => 'required|string|max:4',
+                    'branch_company_name'  => 'string|max:128|nullable',
+                    'branch_company_id'    => 'string|max:8|nullable',
+                    'branch_vat_id'        => 'string|max:12|nullable',
+                    'branch_first_name'    => 'required|string|max:128',
+                    'branch_last_name'     => 'required|string|max:128',
+                    'branch_address'       => 'required|string|max:255',
+                    'branch_address_ext'   => 'string|max:255|nullable',
+                    'branch_postcode'      => 'required|string|max:12',
+                    'branch_city'          => 'required|string|max:128',
+                    'branch_phonecode'     => 'required|string|max:3',
+                    'branch_phone'         => 'required|string|max:9',
+                    'branch_email'         => 'required|string|max:128',
+                ]);
+    
+                if (!$validator->fails()) {
+                    DB::table('address_books_branch')->insert([
+                        'address_book_id'   => $addressBook->id,
+                        'code'              => $data['branch_code'],
+                        'company_name'      => $data['branch_company_name'],
+                        'company_id'        => $data['branch_company_id'],
+                        'vat_id'            => $data['branch_vat_id'],
+                        'first_name'        => $data['branch_first_name'],
+                        'last_name'         => $data['branch_last_name'],
+                        'address'           => $data['branch_address'],
+                        'address_ext'       => $data['branch_address_ext'],
+                        'postcode'          => $data['branch_postcode'],
+                        'city'              => $data['branch_city'],
+                        'phonecode'         => $data['branch_phonecode'],
+                        'phone'             => $data['branch_phone'],
+                        'email'             => $data['branch_email'],
+                    ]);
+                }
+            }
+        }
+        return redirect(route('address-book.index'))->with('message', __('messages.alert.successfully_updated'));
     }
 
     /**
@@ -210,6 +331,9 @@ class AddressBookController extends Controller
      */
     public function destroy(AddressBook $addressBook)
     {
-        //
+        DB::table('address_books_branch')->where('address_book_id', '=', $addressBook->id)->delete();
+        DB::table('address_books_billing')->where('address_book_id', '=', $addressBook->id)->delete();
+        DB::table('address_books')->where('id', '=', $addressBook->id)->delete();
+        return redirect(route('address-book.index'))->with('message', __('messages.alert.removed'));
     }
 }
--- resources/views/address-books/index.blade.php
+++ resources/views/address-books/index.blade.php
@@ -30,6 +30,9 @@
                                     <th class="px-2 pt-4 pb-2 text-center sm:rounded-tl-lg">#</th>
                                     <th class="px-2 pt-4 pb-2 text-left">{{ __('messages.company') }}</th>
                                     <th class="px-2 pt-4 pb-2 text-left">{{ __('messages.first_and_last_name') }}</th>
+                                    <th class="px-2 pt-4 pb-2 text-left">{{ __('messages.city') }}</th>
+                                    <th class="px-2 pt-4 pb-2 text-left">{{ __('messages.email') }}</th>
+                                    <th class="px-2 pt-4 pb-2 text-center">{{ __('messages.phone') }}</th>
                                     <th class="px-2 pt-4 pb-2 sm:rounded-tr-lg"></th>
                                 </tr>
                             </thead>
@@ -37,8 +40,11 @@
                             @foreach ($addressBooks as $addressBook)
                                 <tr data-id="{{ $addressBook->id }}" class="border-b border-gray-100 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 transition duration-400">
                                     <td class="p-2 w-20 min-w-20 text-center">{{ $addressBook->id }}</td>
+                                    <td class="p-2 whitespace-nowrap w-48 min-w-48"></td>
                                     <td class="p-2 whitespace-nowrap"></td>
-                                    <td class="p-2 whitespace-nowrap"></td>
+                                    <td class="p-2 whitespace-nowrap w-48 min-w-48"></td>
+                                    <td class="p-2 whitespace-nowrap w-48 min-w-48"></td>
+                                    <td class="p-2 whitespace-nowrap w-36 min-w-36 text-center"></td>
                                     <td class="p-2 w-24 min-w-24 text-center">
                                         <a href="{{ route('address-book.edit', $addressBook->id) }}" class="inline-block text-gray-300 hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-300 transition duration-400 relative top-0.5 mx-1">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
@@ -48,7 +54,7 @@
                                         <form method="POST" action="{{ route('address-book.destroy', $addressBook->id) }}" class="inline-block relative top-0.5" x-data>
                                             @csrf
                                             @method('DELETE')
-                                            <button type="button" class="text-gray-300 hover:text-pink-600" @click="if(!confirm('{{ __('messages.alert.are_you_sure') }}?')) $event.preventDefault()">
+                                            <button type="submit" class="text-gray-300 hover:text-pink-600" @click="if(!confirm('{{ __('messages.alert.are_you_sure') }}?')) $event.preventDefault()">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                 </svg>
@@ -97,6 +103,9 @@
                                     if ($(this).attr('data-id') == id) {
                                         $(this).find('td').eq(1).text(contactName);
                                         $(this).find('td').eq(2).text(data[i]['first_name'] +' '+ data[i]['last_name']);
+                                        $(this).find('td').eq(3).text(data[i]['city']);
+                                        $(this).find('td').eq(4).text(data[i]['email']);
+                                        $(this).find('td').eq(5).text('+'+ data[i]['phonecode'] +' '+ data[i]['phone']);
                                     }
                                 });                                
                             });
